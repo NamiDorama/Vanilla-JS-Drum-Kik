1ad6f59ba90878b97388ad6ac7b6a36e0e8b0dc4
Yuliia Voitiuk <yulliavoitiuk@gmail.com>
2017-10-03 12:43:32 +0300

    NamiDorama@WIN-MHV9ULFVH29 MINGW64 /d/JavaScript30/Drum Kik (master)

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 function playSound (e) {
 
 	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
@@ -14,12 +15,18 @@ function playSound (e) {
 function removeTransition (e) {
 
 	if(e.propertyName !== 'transform') return; // skip it if it's not a transform
-
 	this.classList.remove('playing');
 }
 
 const keys = document.querySelectorAll('.key');
+
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 window.addEventListener('keydown', playSound);
 
+window.addEventListener('keyup', function(e) {
 
+	const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+	
+	if(!key) return;
+	key.classList.remove('playing');
+});
